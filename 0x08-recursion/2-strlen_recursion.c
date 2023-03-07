6c7b256c00960191f913f42eae2e3102e301ca1a
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-03-07 22:55:31 +0100

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strlen_recursion - know length
+*@s: char
+*
+*Return: size
+*/
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+}
